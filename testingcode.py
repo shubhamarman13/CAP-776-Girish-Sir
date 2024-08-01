97727f101147a8f221f99a44e6acf83b2b5c7adf
shubhamarman13 <shubhamarman13@gmail.com>
2024-08-02 02:40:31 +0530

    updated  the code

--- testingcode.py
+++ testingcode.py
@@ -1,12 +1,5 @@
-"""List= [1,2,3,4,5,6,7,8,33,44,55]
-List[1]=100
-print(List)
-name = "Shubham \r Arman"
-print(name)
-"""
-var1="shubhamArman"
-var2=2234
-print(var1.isalnum())
-print(var1.isalpha())
+List= [1,2,3,4,5,6,7,8,33,44,55]
+print(List.pop())
 
-print(var1.capitalize())
+tup=1 ,2,3,4,5
+print(tup)
