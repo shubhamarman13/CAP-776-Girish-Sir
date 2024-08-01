2fc2da107cf01fc2a53253a5b1e8cb117ff5172e
shubhamarman13 <shubhamarman13@gmail.com>
2024-08-02 00:55:20 +0530

    updating code of list

---
+++ List_first_Program.py
@@ -0,0 +1,5 @@
+List=[1,2,3,4,5,6,7,8,9,10]
+print(List[0:6:-3])
+"""In this problem if we are giving any value with the extende slicing
+then in case of increment is -3 the the start and end are given then
+it returns [] why ? """
--- testingcode.py
+++ testingcode.py
@@ -1,4 +1,12 @@
-print("""i can
-read it
-hole day
-            and i will be happy """)
+"""List= [1,2,3,4,5,6,7,8,33,44,55]
+List[1]=100
+print(List)
+name = "Shubham \r Arman"
+print(name)
+"""
+var1="shubhamArman"
+var2=2234
+print(var1.isalnum())
+print(var1.isalpha())
+
+print(var1.capitalize())
