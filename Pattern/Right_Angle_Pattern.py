a45e1b4556034699053a14029960f6aa959d1b4d
shubhamarman13 <shubhamarman13@gmail.com>
2024-08-03 02:50:27 +0530

    some comment has been updated for better readablity

--- Pattern/Right_Angle_Pattern.py
+++ Pattern/Right_Angle_Pattern.py
@@ -1,6 +1,16 @@
-x=10
-y=1
-for y in range (x):
+""" take input formt  the user and print accordingly this pattern
+*
+**
+***
+****
+*****
+******
+*******
+"""
+
+x=int(input("Enter the Number of Rows  "))
+
+for y in range (x+1):
     print("*"*y)
     y=y+1
 
